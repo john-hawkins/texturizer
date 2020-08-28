350a137567b7ea472c6bf21d53ac4856a5e8aa78
John Hawkins <john.hawkins@john.hawkins.MacBook>
2020-08-28 18:10:21 +1000

    AFINN data processing

---
+++ scripts/process_AFINN_data.py
@@ -0,0 +1,14 @@
+path_to_file = "~/Downloads/AFINN/AFINN-111.txt"
+
+df = pd.read_csv(path_to_file, sep="\t", header=None)
+df.columns = ['word','polarity']
+
+df_pos = df[ df['polarity']>2 ].copy()
+df_pos.drop(['polarity'], inplace=True, axis=1)
+df_pos.to_csv("../texturizer/data/positive.dat",header=False,index=False)
+ 
+df_neg = df[ df['polarity']<-2 ].copy()
+df_neg.drop(['polarity'], inplace=True, axis=1)
+df_neg.to_csv("../texturizer/data/negative.dat",header=False,index=False)
+ 
+
