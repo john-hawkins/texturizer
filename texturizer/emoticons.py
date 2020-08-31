194ce6000f4a97159c82b0b9f53f3633b48707a4
John Hawkins <john.hawkins@john.hawkins.MacBook>
2020-08-31 17:31:57 +1000

    Moving all emoticon lists into data files and extending the range of sentiment matches, adding overall emoticon sentiment indicator

--- texturizer/emoticons.py
+++ texturizer/emoticons.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 import pandas as pd 
 import numpy as np
+import codecs
 import re
 
-emoticons = { "smile":[":-)","(-:",":)","(:"], 
-              "wink":[";-)","(-;",";)","(;"], 
-              "think":[":^)","(^:"], 
-              "laugh":[":-D",":D"], 
-              "tongue":[":-P",":P"], 
-              "sad":[":-(",":(","):",")-:"], 
-              "crying":[":’-(",")-`:",":'-(",")-':"], 
-              "happycry":[":’-)","(-`:",":’)","(`:",":'-)","(-':"]
-            }
+from .process import load_word_list
+from .process import load_word_pattern
 
-fwd_regex = "[:;]['’`]{0,1}[-=^]{0,2}[DP@*\\|/()<>{}\[\]]{1,2}"
+smiles = load_word_list("emoticons-smile.dat")
+laughs = load_word_list("emoticons-laugh.dat")
+winks = load_word_list("emoticons-wink.dat")
+cheekys = load_word_list("emoticons-wink.dat")
+kisses = load_word_list("emoticons-kiss.dat")
+happycrys = load_word_list("emoticons-happy-cry.dat")
+crys = load_word_list("emoticons-cry.dat")
+sads = load_word_list("emoticons-sad.dat")
+shocks = load_word_list("emoticons-shock.dat")
+sceptics = load_word_list("emoticons-sceptical.dat")
+ 
+fwd_regex = "[:;8BX]['’`]{0,1}[-=^oc]{0,2}[DPO0J3ox,Þþb@*\\|/()<>{}\[\]]{1,2}"
 fwd_re = re.compile(fwd_regex)
 bck_regex = "[@*\\|/()<>{}\[\]]{1,2}[-=^]{0,2}['’`]{0,1}[:;]"
 bck_re = re.compile(bck_regex)
 
+ 
 """
     texturizer.emoticons: Emoticon Recognition Text Features
  
@@ -30,22 +36,23 @@ bck_re = re.compile(bck_regex)
      For example emoticons: 8) or (B will not be matched.
 
     To avoid matching characters inside document markup language tags there is a 
-    rudimentary regex based tag removal. This will remove URLS and HTML tags before
-    trying to match emoticons
+    rudimentary regex based tag removal and unescaped version of the text that is
+    expecte to have been generated in the intial simple text function run by the program. 
+    This will remove URLS and HTML tags before trying to match emoticons.
 
     Some references used when considering which empticons to include:
 
     https://www.unglobalpulse.org/2014/10/emoticon-use-in-arabic-spanish-and-english-tweets/
 
-https://www.researchgate.net/publication/266269913_From_Emoticon_to_Universal_Symbolic_Signs_Can_Written_Language_Survive_in_Cyberspace
+    https://www.researchgate.net/publication/266269913_From_Emoticon_to_Universal_Symbolic_Signs_Can_Written_Language_Survive_in_Cyberspace
 
-https://www.sciencedirect.com/science/article/abs/pii/S0950329317300939
+    https://www.sciencedirect.com/science/article/abs/pii/S0950329317300939
 
-https://www.semanticscholar.org/paper/An-Approach-towards-Text-to-Emoticon-Conversion-and-Jha/3b81505fa7fec81563b2dafae3939fa1b07f3a98
+    https://www.semanticscholar.org/paper/An-Approach-towards-Text-to-Emoticon-Conversion-and-Jha/3b81505fa7fec81563b2dafae3939fa1b07f3a98
 
-https://www.qualitative-research.net/index.php/fqs/article/view/175/391
+    https://www.qualitative-research.net/index.php/fqs/article/view/175/391
 
-https://www.researchgate.net/publication/221622114_M_Textual_Affect_Sensing_for_Sociable_and_Expressive_Online_Communication
+    https://www.researchgate.net/publication/221622114_M_Textual_Affect_Sensing_for_Sociable_and_Expressive_Online_Communication
 
 """
 
@@ -57,8 +64,6 @@ def add_text_emoticon_features(df, columns):
         Add features that detect the presence of emoticons.
     """
     rez = df.copy()
-    if len(columns) == 0:
-        columns = get_text_column_names(df)
     for col in columns:
         rez = add_emoticon_features(rez, col)
     return rez
@@ -72,41 +77,55 @@ def add_emoticon_features(df, col):
         that indicate both the presence and emotional flavour of the emoticon.
     """
     def cal_features(x, col):
-        emo_match = 0 
+        emos = 0 
         smiley = 0 
         wink = 0 
-        sad = 0
+        kiss = 0
         happycry = 0
         laugh = 0
-        tongue = 0
+        cheeky = 0
         crying = 0
+        sad = 0
+        shock = 0 
+        sceptic = 0 
         if x[col]==x[col]:
-            text = remove_urls_and_tags(x[col].lower())
+            text = x[col + "_cleaned"] 
             matches = fwd_re.findall(text)
             bck_matches = bck_re.findall(text)
             if len(matches)>0 or len(bck_matches)>0:
                 matches.extend(bck_matches)
-                emo_match = len(matches) 
-                if set(matches).intersection(emoticons['smile']):                
+                emos = len(matches)
+                if set(matches).intersection( smiles ):                
                     smiley = 1
-                if set(matches).intersection(emoticons['crying']):                
+                if set(matches).intersection( crys ):        
                     crying = 1
-                if set(matches).intersection(emoticons['wink']):                
+                if set(matches).intersection( winks ):                
                     wink = 1
-                if set(matches).intersection(emoticons['sad']):                
+                if set(matches).intersection( kisses ):                
+                    kiss = 1
+                if set(matches).intersection( sads ):                
                     sad = 1
-                if set(matches).intersection(emoticons['laugh']):                
+                if set(matches).intersection( shocks ):                
+                    shock = 1
+                if set(matches).intersection( sceptics ):                
+                    sceptic = 1
+                if set(matches).intersection( laughs ):                
                     laugh = 1
-                if set(matches).intersection(emoticons['tongue']):                
-                    tongue = 1
-                if set(matches).intersection(emoticons['happycry']):                
+                if set(matches).intersection( cheekys ):                
+                    cheeky = 1
+                if set(matches).intersection( happycrys ):                
                     happycry = 1
-        return emo_match, smiley, wink, sad, crying, tongue, laugh, happycry
+        pos = smiley + wink + kiss + happycry + laugh + cheeky
+        neg = crying + sad + shock + sceptic
+        return emos,smiley,wink,kiss,happycry,laugh,cheeky,crying,sad,shock,sceptic,pos,neg
 
-    df[[col+'_emoticons', col+'_emo_smiley', col+'_emo_wink', col+'_emo_sad', col+'_emo_cry', col+'_emo_tongue', col+'_emo_laugh', col+'_emo_happycry']] = df.apply(cal_features, col=col, axis=1, result_type="expand")
+    df[[col+'_emoticons', col+'_emo_smiley', col+'_emo_wink', col+'_emo_kiss', col+'_emo_happycry', col+'_emo_laugh', col+'_emo_cheeky', col+'_emo_cry', col+'_emo_sad', col+'_emo_shock', col+'_emo_sceptic', col+'_emo_pos', col+'_emo_neg']] = df.apply(cal_features, col=col, axis=1, result_type="expand")
+
+    df[col+'_emo_sentiment'] = (df[col+'_emo_pos'] - df[col+'_emo_neg'] )
 
     return df
 
+########################################################################################
 def remove_urls_and_tags(text):
     """
         Necessary to avoid common false positive emoticon matches
@@ -117,6 +136,12 @@ def remove_urls_and_tags(text):
         new_text = re.sub(p, ' ', new_text)
     return new_text
 
+def remove_escapes_and_non_printable(text):
+    new_text0 = codecs.escape_decode(text)[0].decode("utf-8")
+    pattern = "\0|\n|\r|\b|\t|\f|\v"
+    new_text1 = re.sub(pattern, " ", new_text0)
+    return new_text1
+
 ########################################################################################
 def null_tolerant_len(x):
     if x != x:
@@ -124,8 +149,4 @@ def null_tolerant_len(x):
     else:
         return len(x)
 
-########################################################################################
-def get_text_column_names(df):
-    rez = []
-    return rez
 
